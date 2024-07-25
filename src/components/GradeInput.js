97018e4d22a6e11543efbdbc63fd6ff7168273db
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-25 16:25:52 +0530

    grade commit

---
+++ src/components/GradeInput.js
@@ -0,0 +1,22 @@
+import { React } from 'react';
+const GradeInput = (context) => {
+	const {
+		actions: { setStudentGrade },
+		state: { grade },
+	} = context;
+
+	return <div>
+		<label> Grade : </label>
+		<select
+			type="radio"
+			value={ grade }
+			onChange={ (event) => setStudentGrade(event.target.value) }
+		>
+			<option value="">Select Grade</option>
+			<option value="11">11</option>
+			<option value="12">12</option>
+		</select>
+	</div>;
+};
+
+export default GradeInput;
