ae398857cea1b48cb31b2ff96c9a50f9d8d74cdf
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-25 16:32:32 +0530

    Name INput

---
+++ src/components/NameInput.js
@@ -0,0 +1,17 @@
+import { React } from 'react';
+const NameInput = (context) => {
+	const { actions: { setStudentName }, state: { name }}
+ = context;
+
+	return <div>
+		<label> Student Name : </label>
+		<input
+			type="text"
+			value={ name }
+			placeholder="Enter the Full Name"
+			onChange={ (event) => setStudentName(event.target.value) }
+		/>
+	</div>;
+};
+
+export default NameInput;
