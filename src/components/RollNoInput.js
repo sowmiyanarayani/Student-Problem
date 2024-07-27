5e6ffe49a931a4d482ebe2c97b0fac66737bb180
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-27 13:16:52 +0530

    RollNo

--- src/components/RollNoInput.js
+++ src/components/RollNoInput.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { React } from 'react';
 const RollNoInput = (context) => {
 	const { state: { rollNo }, actions: { setRollNo }} = context;
 
 	return <div>
-		<label>Roll Number : </label>
-		<input
+		<label>Roll Number :{ } 		<input
 			type="number"
 			placeholder="enter the Roll number"
 			value={ rollNo }
 			onChange={ (e) => setRollNo(e.target.value) }
-		/>
+		                          />
+		</label>
+
 	</div>;
 };
 
